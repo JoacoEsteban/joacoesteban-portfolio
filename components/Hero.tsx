49023a25f87fed1c34eac41b3f24abfec3ba5b36
Joaco Esteban <dev.joaco.esteban@gmail.com>
2021-07-28 23:48:23 -0300

    IMPROVE, fonts, styles, components

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+import { Container, Heading, Text } from '@chakra-ui/react'
+// import { Button, Flex } from '@chakra-ui/react'
+export default function Hero () {
+  return (
+    <div>
+      <Container>
+        <Heading as={'h6'}>
+          <Text align={'center'}>
+            Full-Stack Web Developer
+          </Text>
+        </Heading>
+        Joaco Esteban
+      </Container>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import { Button, Flex } from '@chakra-ui/react'
+import Hero from '../components/Hero'
 
 export default function Home () {
   return (
-    <title>Joaco Esteban</title>
+    <div>
+      <title>
+        Joaco Esteban - Full-Stack Web Developer
+      </title>
+      <Hero></Hero>
+    </div>
   )
 }
