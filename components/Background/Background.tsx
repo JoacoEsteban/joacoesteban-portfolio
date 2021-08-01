cc98001b7107cb1a594b20c18a923ab7fc26b6a1
Joaco Esteban <dev.joaco.esteban@gmail.com>
2021-07-31 22:59:31 -0300

    IMPROVE, background-illustrations

--- components/Background/Background.tsx
+++ components/Background/Background.tsx
@@ -1,19 +1,25 @@
 import { Box, Container, Heading, Text } from '@chakra-ui/react'
 import styles from './Background.module.scss'
+import Illustrations from './Illustrations'
 
 export default function Hero () {
   return (
-    <div className={styles.background}>
-      <div className={styles.titlesContainer}>
-        <Container>
-          {
-            [...Array(11)].map((e, i) =>
-              <div key={i} className={styles.backgroundTitle}>
-                Full Stack Developer
-              </div>
-            )
-          }
-        </Container>
+    <div>
+      <div className={[styles.background, styles.illustrationsContainer].join(' ')}>
+        <Illustrations></Illustrations>
+      </div>
+      <div className={styles.background}>
+        <div className={styles.titlesContainer}>
+          <Container>
+            {
+              [...Array(11)].map((e, i) =>
+                <div key={i} className={styles.backgroundTitle}>
+                  Full Stack Developer
+                </div>
+              )
+            }
+          </Container>
+        </div>
       </div>
     </div>
   )
---
+++ components/Background/Illustrations.tsx
@@ -0,0 +1,24 @@
+export default function Illustrations () {
+  return (
+    <svg width="100%" height="100%" viewBox="0 0 1988 1329" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M415.825 244.736C158.565 427.703 218.143 560.955 313.621 651.107C408.621 702.107 377.275 872.028 374.621 1014.11C371.519 1180.15 1030.53 476.37 1032.74 470.596C1034.95 464.822 1071.24 285.038 840.559 368.583C609.881 452.128 737.4 16.028 415.825 244.736Z"
+        fill="url(#1922d955cdb0_paint0_linear)" />
+      <path
+        d="M1268.62 411.606C1230.12 777.106 1041.62 905.606 1041.62 905.606C1041.62 905.606 852.121 996.606 1032.12 1078.61C1183.25 1147.46 1804.63 883.517 1851.12 759.606C1868 714.617 1950.8 404.061 1720.12 487.606C1489.44 571.151 1309.96 19.1664 1268.62 411.606Z"
+        fill="url(#1922d955cdb0_paint1_linear)" />
+      <defs>
+        <linearGradient id="1922d955cdb0_paint0_linear" x1="386.239" y1="264.847" x2="779.183" y2="703.708"
+          gradientUnits="userSpaceOnUse">
+          <stop stop-color="#00D1FF" />
+          <stop offset="1" stop-color="#D800B5" />
+        </linearGradient>
+        <linearGradient id="1922d955cdb0_paint1_linear" x1="1241.24" y1="447.847" x2="1634.18" y2="886.707"
+          gradientUnits="userSpaceOnUse">
+          <stop stop-color="#BDFF00" />
+          <stop offset="1" stop-color="#D80000" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
