af3cc2d6ee5819c8c53881af0dad118c09faa492
Joaco Esteban <dev.joaco.esteban@gmail.com>
2021-08-01 00:05:05 -0300

    IMPROVE, links

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,9 @@
-import { Box, Container, Heading, Text } from '@chakra-ui/react'
+import { Box, Container, Heading, Text, Link } from '@chakra-ui/react'
 
 const Bold = ({ children }) => <Text fontWeight={410} display="inline">{children}</Text>
+const Link_ = ({ children, href }) => <Link className="clickable-effects no-hover-fx" href={href} target="_blank" rel="noreferrer">{children}</Link>
+const CoderLink = ({ children }) => <Link_ href="https://www.coderhouse.com">{children}</Link_>
+const SbLink = ({ children }) => <Link_ href="https://songbasket.com">{children}</Link_>
 
 export default function Hero () {
   return (
@@ -14,8 +17,8 @@ export default function Hero () {
         <Box fontWeight={'regular'} fontSize={'xl'}>
           <Text>
             Full Stack Web Developer based on <Bold>Buenos Aires, Argentina</Bold> <br />
-            Full-Time Developer at <Bold>CoderHouse</Bold> <br />
-            Currently working on <Bold>SongBasket</Bold>, The all in one solution to turn Spotify playlists into MP3 <br />
+            Full-Time Developer at <CoderLink><Bold>CoderHouse</Bold></CoderLink> <br />
+            Currently working on <SbLink><Bold>SongBasket</Bold>, The all in one solution to turn Spotify playlists into MP3 </SbLink><br />
           </Text>
         </Box>
       </Container>
