71ef58b361d046079fb82648483748981d7813c8
Joaco Esteban <dev.joaco.esteban@gmail.com>
2021-07-31 22:49:00 -0300

    IMPROVE, background titles

--- components/Background/Background.tsx
+++ components/Background/Background.tsx
@@ -1,10 +1,20 @@
-import { Box, Heading, Text } from '@chakra-ui/react'
+import { Box, Container, Heading, Text } from '@chakra-ui/react'
 import styles from './Background.module.scss'
-
 
 export default function Hero () {
   return (
     <div className={styles.background}>
+      <div className={styles.titlesContainer}>
+        <Container>
+          {
+            [...Array(11)].map((e, i) =>
+              <div key={i} className={styles.backgroundTitle}>
+                Full Stack Developer
+              </div>
+            )
+          }
+        </Container>
+      </div>
     </div>
   )
 }
