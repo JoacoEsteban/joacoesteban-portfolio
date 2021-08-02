1f0078c089775521dd7c9d941dac522984cc57c8
Joaco Esteban <dev.joaco.esteban@gmail.com>
2021-08-02 02:26:24 -0300

    IMPROVE, background styles & repeating titles based on screen size

--- components/Background/Background.tsx
+++ components/Background/Background.tsx
@@ -1,26 +1,57 @@
 import { Box, Container, Heading, Text } from '@chakra-ui/react'
+import React, { Component, forwardRef, Fragment, MutableRefObject, RefObject, useRef } from 'react'
 import styles from './Background.module.scss'
 import Illustrations from './Illustrations'
 
-export default function Hero () {
-  return (
-    <div>
-      <div className={[styles.background, styles.illustrationsContainer].join(' ')}>
-        <Illustrations></Illustrations>
-      </div>
-      <div className={styles.background}>
-        <div className={styles.titlesContainer}>
-          <Container>
-            {
-              [...Array(11)].map((e, i) =>
-                <div key={i} className={styles.backgroundTitle} style={{ '--_index': i } as React.CSSProperties}>
-                  Full Stack Developer
-                </div>
-              )
-            }
-          </Container>
+
+
+const Title = ({ index }: { index?: number }) => (
+  <div className={styles.backgroundTitle} style={{ '--_index': index || 0 } as React.CSSProperties}>Full Stack Developer</div>
+)
+
+export default class Background extends Component {
+  private firstTitleRef: MutableRefObject<null>
+  private state: { mounted: boolean, titlesAmount: number }
+
+  constructor(props: {}) {
+    super(props)
+    this.firstTitleRef = React.createRef()
+    this.state = {
+      mounted: false,
+      titlesAmount: 0
+    }
+    console.log(this.firstTitleRef)
+  }
+
+  getTitlesAmount (): number {
+    const value = Math.ceil(window.innerHeight / this.firstTitleRef.current.clientHeight) - 1
+    console.log(value)
+    return value
+  }
+
+  componentDidMount () {
+    this.setState({ mounted: true, titlesAmount: this.getTitlesAmount() })
+  }
+
+  render () {
+    // eslint-disable-next-line react/display-name
+    const FwTitle = forwardRef((props, ref) => <div ref={this.firstTitleRef}><Title /></div>)
+    return (
+      <div>
+        <div className={[styles.background, styles.illustrationsContainer].join(' ')}>
+          <Illustrations></Illustrations>
+        </div>
+        <div className={styles.background}>
+          <div className={styles.titlesContainer}>
+            <Container>
+              <FwTitle ref={this.firstTitleRef} />
+              {
+                this.state.mounted && [...Array(this.state.titlesAmount || 0)].map((itm, i) => <Title key={i} index={i + 1} />)
+              }
+            </Container>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
