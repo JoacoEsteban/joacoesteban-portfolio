7f45ed62702dc32b165f695645d9dcf44c9154c2
Joaco Esteban <dev.joaco.esteban@gmail.com>
2023-09-22 15:14:39 +0200

    FEAT. SongBasket icon Name alias

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -14,14 +14,14 @@ const Link_ = ({ children, href }: props & { href: string }) => <Link className=
 const CoderLink = ({ children }: props) => <Link_ href="https://www.coderhouse.com">{children}</Link_>
 const SbLink = ({ children }: props) => <Link_ href="https://songbasket.com">{children}</Link_>
 const ConstitucionLink = ({ children }: props) => <Link_ href="https://constitucion.ar">{children}</Link_>
-const TechImg = ({ src, alt }: { src: string, alt: string }) => (
+const Icon = ({ src, alt }: { src: string, alt: string }) => (
   <Box marginLeft={2} marginRight={2} className={styles.centerY}>
     <img src={src} alt={alt} title={alt} width={16} height={16} />
   </Box>
 )
 const Tech = (technology: Technology) => (
   <Link_ href={technology.website}>
-    <TechImg src={technology.icon} alt={technology.name} />
+    <Icon src={technology.icon} alt={technology.name} />
   </Link_>
 )
 
@@ -30,17 +30,25 @@ export default function Hero () {
   return (
     <div className={styles.heroContainer}>
       <Container margin="auto" paddingBottom="6">
-        <Heading as={'h1'} size={'3xl'} fontWeight={'medium'} marginBottom={5}>
+        <Heading as={'h1'} size={'3xl'} fontWeight={'medium'} marginBottom={2}>
           <span>
             Joaquin Esteban
           </span>
         </Heading>
+        <Box fontWeight={'medium'} fontSize={'xl'} marginBottom={5}>
+          <span>
+            <i>Joaco (/'xoa.ko/.)</i>
+          </span>
+        </Box>
+
         <Box fontWeight={'regular'} fontSize={'xl'}>
           <span>
             Full Stack Web Developer based in <Bold>Buenos Aires, Argentina</Bold> <br />
-            Full-Time Developer in <CoderLink><Bold>Coderhouse</Bold></CoderLink> <br />
+            Full-Time Developer at <CoderLink><Bold>Coderhouse</Bold></CoderLink> <br />
             Hosting <ConstitucionLink><Bold>Constitucion.ar</Bold></ConstitucionLink><br />
-            Currently working on <SbLink><Bold>SongBasket</Bold>, The all in one solution to turn Spotify playlists into MP3 </SbLink><br />
+            <Flex>
+              Currently dropping free time on <SbLink><Icon src='/assets/icons/songbasket.png' alt='SongBasket' /><Bold>SongBasket</Bold>, The all in one solution to turn Spotify playlists into MP3 </SbLink><br />
+            </Flex>
           </span>
         </Box>
       </Container>
