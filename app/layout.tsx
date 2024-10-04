48c52d272287c6948f62215855e5cf02e3c834a5
Joaquin Esteban <dev@joacoesteban.com>
2024-10-04 23:03:38 +0200

    ADD, analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
+import { GoogleAnalytics } from '@next/third-parties/google'
 import '../styles/globals.scss'
 import '../styles/fonts/MADE Tommy/stylesheet.scss'
 import { Providers } from './providers'
-import "@fontsource/dm-sans/400.css"
-import "@fontsource/dm-sans/500.css"
+import '@fontsource/dm-sans/400.css'
+import '@fontsource/dm-sans/500.css'
 
 export const metadata = {
   title: 'Joaquin Esteban - Software Engineer | Full-Stack Web Developer',
-  description: 'Software Engineer based in Torino, Italy. Currently working on Vind, Map keys to on-screen elements and trigger clicks effortlessly and SongBasket, The all in one solution to turn Spotify playlists into MP3',
+  description:
+    'Software Engineer based in Torino, Italy. Currently working on Vind, Map keys to on-screen elements and trigger clicks effortlessly and SongBasket, The all in one solution to turn Spotify playlists into MP3',
 }
 
-export default function RootLayout ({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
@@ -19,6 +21,7 @@ export default function RootLayout ({
       <body>
         <Providers>{children}</Providers>
       </body>
+      <GoogleAnalytics gaId={'G-Q3MX5BCNHT'} />
     </html>
   )
 }
