8a01d44d6e323145fbdc2dc1c7b5bc1b994bc9ed
Joaquin Esteban <dev@joaco.io>
2026-03-23 23:57:13 -0300

    chore: add adsense verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-8407180754020844" />
+      </head>
       <body>
         <Providers>{children}</Providers>
         <Analytics />
