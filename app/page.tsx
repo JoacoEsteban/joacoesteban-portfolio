5b70f5d82d2c906bdd354f7f837e385b20c41b4e
Joaquin Esteban <dev@joaco.io>
2026-04-20 02:26:56 -0300

    fix: layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Hero from '../components/Hero/Hero'
 
 export default function Index() {
   return (
-    <div className="max-w-[80em] mx-auto">
-      <div className="flex flex-col h-screen relative">
+    <div className="max-w-[80em] mx-auto h-full">
+      <div className="flex flex-col h-full relative">
         <title>
           Joaco Esteban - Software Engineer | Full-Stack Web Developer
         </title>
--- components/ClientRoot.tsx
+++ components/ClientRoot.tsx
@@ -5,10 +5,12 @@ import Header from './Header/Header'
 
 export default function ClientRoot({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Background />
       <Header />
-      {children}
-    </>
+      <main className="flex-1 min-h-0">
+        {children}
+      </main>
+    </div>
   )
 }
